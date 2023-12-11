c7508d09bd1bfde118c1924341088a8a93994686
juratali <juratbekxudayberganov9@gmail.com>
2023-12-11 11:26:33 +0500

    changes on progress bar

--- app/provider.tsx
+++ app/provider.tsx
@@ -4,11 +4,21 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { SessionProvider } from "next-auth/react";
+import NextTopLoader from "nextjs-toploader";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
 export function Provider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
-      <SessionProvider>{children}</SessionProvider>
+      <SessionProvider>
+        {children}
+        <ProgressBar
+          height="4px"
+          color="#2299DD"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
+      </SessionProvider>
     </NextThemesProvider>
   );
 }
